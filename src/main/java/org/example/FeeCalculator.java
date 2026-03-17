8b867549130d61cad9c60ead07882cc1803460b9
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2026-03-17 16:17:55 +0100

    (ref) : simplification des if dans la méthode calculateFee

--- src/main/java/org/example/FeeCalculator.java
+++ src/main/java/org/example/FeeCalculator.java
@@ -8,17 +8,21 @@ public class FeeCalculator {
         double fee = 0;
 
         // calculate price for adults
-        if ((visitor.age > 14) && (TicketType.HALF_DAY == ticketType)) {
-            fee = ADULT_PRICE_BASE * 0.6;
-        } else if ((visitor.age > 14) && (TicketType.FULL_DAY == ticketType)) {
-            fee = ADULT_PRICE_BASE * 1.2;
+        if (visitor.age > 14) {
+            if (TicketType.HALF_DAY == ticketType) {
+                fee = ADULT_PRICE_BASE * 0.6;
+            } else if (TicketType.FULL_DAY == ticketType) {
+                fee = ADULT_PRICE_BASE * 1.2;
+            }
         }
 
         // calculate price for children
-        if ((visitor.age <= 14) && (TicketType.HALF_DAY == ticketType)) {
-            fee = CHILD_PRICE_BASE * 0.2;
-        } else if ((visitor.age <= 14) && (TicketType.FULL_DAY == ticketType)) {
-            fee = CHILD_PRICE_BASE * 0.5;
+        if (visitor.age <= 14) {
+            if (TicketType.HALF_DAY == ticketType) {
+                fee = CHILD_PRICE_BASE * 0.2;
+            } else if (TicketType.FULL_DAY == ticketType) {
+                fee = CHILD_PRICE_BASE * 0.5;
+            }
         }
         return fee;
     }
